9809a4339392ccb66b4a188cdad3897b8f2cb7b4
Grey Pegram <dpegram85@gmail.com>
2024-01-27 19:20:30 -0500

    Completed Labs

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    return fetch('http://localhost:3000/users/', {
+        method: 'POST',
+        headers: {
+            "Content-Type" : "application/json",
+            "Accept" : "application/json",
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail,
+        })
+    })
+    .then(resp => resp.json())
+    .then(data => {
+        const newID = data.id
+        const newElement = document.createElement('p')
+        newElement.textContent = `${newID}`
+        document.body.appendChild(newElement)
+    })
+    .catch(error => {
+        const errElement = document.createElement('p')
+        errElement.textContent = `${error.message}`
+        document.body.appendChild(errElement)
+    })
+}
